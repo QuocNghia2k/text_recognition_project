bebef24e6b6066d3bfb05a7ad521f8f03b3824e4
nghiaho <nghiaho@lecle.co.kr>
2023-05-23 23:05:59 +0700

    add firebase in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:async';
+
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'firebase_options.dart';
 
 import 'app.dart';
 import 'blocs/blocs.dart';
@@ -8,12 +16,38 @@ import 'dependencies/app_dependencies.dart';
 import 'enums.dart';
 
 Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
+  runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
 
-  await AppDependencies.init(env: Environment.dev);
-  runApp(App(bloc: GetIt.I.get<AppBloc>()));
+      // Pass all uncaught "fatal" errors from the framework to Crashlytics
+      FlutterError.onError = (errorDetails) {
+        FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+      };
+      // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+      PlatformDispatcher.instance.onError = (error, stack) {
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+        return true;
+      };
+
+      await SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ]);
+
+      await AppDependencies.init(env: Environment.dev);
+      runApp(
+        DevicePreview(
+          builder: (_) => App(bloc: GetIt.I.get<AppBloc>()),
+          enabled: false,
+        ),
+      );
+    },
+    (error, stack) {
+      // FirebaseCrashlytics.instance.recordError(error, stack);
+    },
+  );
 }
